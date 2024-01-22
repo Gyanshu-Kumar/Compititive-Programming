90c4762cdf780135cd23d284c98487ff1e2c2b9a
Gyanshu Kumar <119155250+Gyanshu-Kumar@users.noreply.github.com>
2024-01-22 23:35:05 +0530

    Update Birthday_invitation.cpp

--- Birthday_invitation.cpp
+++ Birthday_invitation.cpp
@@ -1,3 +1,16 @@
+// Cherry's Birthday is coming this month: She want to plan a birthday party and is preparing a invite list with her friend Aashi.She ask to tell her name to add in the list.
+
+// Aashi is a random guy and keeps coming with names of people randomly to add in the invite list, even if the name is already in the list.
+// Cherry hates redudancy and hence, enlist the names only once.
+
+// find the final invite list, that contains names without any repetition.
+
+// Input:
+// First Line of each Test Contains an integer N, the number of names that aashi pops up.
+
+// Output:
+// Output the final invite list with each name in the final invite list are lexicographically.
+//________________________________________________________CODE___________________________________________________________________________________________________________
 #include<iostream>
 #include<set>
 #include<vector>
